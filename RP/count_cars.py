59f2a9c55218995b0de8f49a3cd158d304454583
Isaac Eliassi <isaac.eliassi@sap.com>
2016-02-10 10:36:48 +0200

    add util function

---
+++ RP/count_cars.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# General imports
+import RPi.GPIO as GPIO
+
+import utils as u
+
+# Which GPIO's are used [0]=BCM Port Number [1]=BCM Name [2]=Use [3]=Pin
+# ----------------------------------------------------------------------
+GPIO_ECHO = 17
+GPIO_TRIG = 4
+
+def distance(GPIO_ECHO,GPIO_TRIG):
+    u.debug_print ("GPIO_TRIG = " + str(GPIO_TRIG) + ",GPIO_ECHO = " + str(GPIO_ECHO))
+    # Set GPIO Channels
+    # -----------------
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(GPIO_TRIG, GPIO.OUT)
+    GPIO.output(GPIO_TRIG, False)
+
+
+    # A couple of variables
+    # ---------------------
+    EXIT = 0                        # Infinite loop
+    decpulsetrigger = 0.0001        # Trigger duration
+    inttimeout = 1000               # Number of loop iterations before timeout called
+
+
+    # Wait for 2 seconds to allow the ultrasonics to settle (probably not needed)
+    # ---------------------------------------------------------------------------
+    #print "Waiting for 2 seconds....."
+    #time.sleep(2)
+
+
+    # Go
+    # --
+    debug_print("Running....")
+    min_dist = 100
+
+    # Never ending loop
+    # -----------------
+    while EXIT < 10:
+
+        # Trigger high for 0.0001s then low
+        GPIO.output(GPIO_TRIG, True)
+        time.sleep(decpulsetrigger)
+        GPIO.output(GPIO_TRIG, False)
+
+        # Wait for echo to go high (or timeout)
+        i_countdown = inttimeout
+
+        while (GPIO.input(GPIO_ECHO) == 0 and i_countdown > 0):
+            i_countdown -=  1
+
+        # If echo is high than the i_countdown not zero
+        if i_countdown > 0:
+
+            # Start timer and init timeout countdown
+            echostart = time.time()
+            i_countdown = inttimeout
+
+            # Wait for echo to go low (or timeout)
+            while (GPIO.input(GPIO_ECHO) == 1 and i_countdown > 0):
+                i_countdown -= 1
+
+            # Stop timer
+            echoend = time.time()
+
+
+            # Echo duration
+            echoduration = echoend - echostart
+
+        # Display distance
+        if i_countdown > 0:
+            i_distance = (echoduration*1000000)/58
+            debug_print("Distance = " + str(i_distance) + "cm")
+            min_dist = min(min_dist,i_distance)
+        else:
+            debug_print("Distance - timeout")
+
+            # Wait at least .01s before re trig (or in this case .1s)
+            time.sleep(.1)
+
+        EXIT +=1
+        return min_dist
+
+
+#__main__
+u.debug_print("Hello WeroJam")
+
---
+++ RP/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+debug = True
+
+def debug_print(my_line,must_print = False):
+    if debug or must_print:
+        print my_line
